421718defcb813122b9eb2ba8bd10ccf63b41487
JNeto <joaomendes15@gmail.com>
2023-05-19 14:07:50 +0100

    ex 2 done

---
+++ PlayerManager4/CompareByName.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace PlayerManager4
+{
+    public class CompareByName: IComparer<Player>
+    {
+
+        private bool _ascendantMode;
+        
+        public CompareByName(bool ascendantMode)
+        {
+            _ascendantMode = ascendantMode;
+        }
+        
+        public int Compare(Player? x, Player? y)
+        {
+            List<Player> players = new List<Player>() { x, y };
+            int comparison = x.Name.CompareTo(y.Name);
+            
+            if (comparison == 0)
+                return 0;
+
+            if (comparison == 1 )
+            {
+                if (_ascendantMode)
+                    return 1;
+                else
+                    return -1;
+            }
+            
+            if (_ascendantMode)
+                return -1;
+            else
+                return 1;
+            
+        }
+    }
+}
---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace PlayerManager4
+{
+    public class Player: IComparable<Player>
+    {
+        public string Name { get; }
+        public int Score { get; }
+        
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+        
+        public int CompareTo(Player? other)
+        {
+            if (other.Score == Score)
+                return 0;
+            if (other.Score < Score)
+                return -1;
+            return 1;
+        }
+    }
+}
