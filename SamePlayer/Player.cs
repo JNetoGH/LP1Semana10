2bd41f72b1541f4e08bd9617342bf9786af395b1
JNeto <joaomendes15@gmail.com>
2023-05-19 14:37:55 +0100

    ex 6 done

--- SamePlayer/Player.cs
+++ SamePlayer/Player.cs
@@ -4,5 +4,18 @@
     {
         public PlayerType Type { get; set; }
         public string Name { get; set; }
+        
+        public override int GetHashCode()
+        {
+            return Type.GetHashCode() ^ Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            Player otherPlayer = (Player)obj;
+            return Type == otherPlayer.Type && Name == otherPlayer.Name;
+        }
     }
 }
--- SamePlayer/Program.cs
+++ SamePlayer/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SamePlayer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HashSet<Player> setOfPlayers = new HashSet<Player>();
+
+            Player player1 = new Player { Type = PlayerType.Tank, Name = "Ana" };
+            Player player2 = new Player { Type = PlayerType.Slayer, Name = "Paulo" };
+            Player player3 = new Player { Type = PlayerType.Tank, Name = "Ana" };
+
+            setOfPlayers.Add(player1);
+            setOfPlayers.Add(player2);
+            setOfPlayers.Add(player3);
+
+            foreach (Player p in setOfPlayers)
+            {
+                Console.WriteLine($"{p.Name} is a {p.Type}");
+            }
         }
     }
 }
